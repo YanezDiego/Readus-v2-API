72455afb414e50390ffb90389e496ff9b9aa68c2
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-18 00:52:39 -0400

    fix spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
     validates :last_name, presence: true
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
-    validates :password, presence: true, length: {in: 6..20}
+    validates :password, presence: true, length: { in: 6..20 }
 end
