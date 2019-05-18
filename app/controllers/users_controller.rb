c8732e313a249a3683b6544ff707009ba8326d1e
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-18 00:53:14 -0400

    fix spacing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [ :show, :edit ]
+
+    private
+
+    def set_user
+        @user = User.find_by(id: params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+    end
 end
