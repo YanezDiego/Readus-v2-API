18759451875bfe2392a044d7b8302d933a8a2cde
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-18 00:25:03 -0400

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
 
-    
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: {in: 6..20}
 end
