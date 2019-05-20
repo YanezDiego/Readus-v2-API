dddd65443a85fd8915e40d1e328268b68d6d2c4c
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-20 13:41:38 -0400

    add show & create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [ :show, :edit ]
+    #before_action :authenticate_user
+    #before_action :set_user, only: [ :show, :edit ]
+
+
+    def create
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            render json: @user
+        else
+            render json: error
+        end
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+            render json: @user
+    end
 
     private
 
