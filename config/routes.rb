1d92c94db25131529f5e7bf505d01fc2a3f6f65b
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-20 13:41:58 -0400

    remove namespace api

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  namespace :api do
     resources :users
     post 'user_token' => 'user_token#create'
-  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
