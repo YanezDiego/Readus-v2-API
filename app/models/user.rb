abbe06a56c3a5222c493e5e2373055e83193e6c4
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-20 13:53:11 -0400

    user and movie association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: { in: 6..20 }
+
+    has_many :movies
 end
