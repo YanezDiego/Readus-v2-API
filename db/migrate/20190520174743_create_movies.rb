11697445b36c468302e36f61bc1ab5271ea3dea6
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-22 15:40:53 -0400

    add img url column

--- db/migrate/20190520174743_create_movies.rb
+++ db/migrate/20190520174743_create_movies.rb
@@ -2,9 +2,11 @@ class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
       t.string "display_title"
+      t.string "img_src"
       t.string "byline"
       t.string "headline"
       t.string "summary_short"
+      t.string "movie_url"
       t.integer "user_id"
       t.timestamps
     end
