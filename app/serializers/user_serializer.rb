e011f4a24fd214a60f1a8bf7d9548da832ea5952
Diego Yanez <diegosyanez92@gmail.com>
2019-06-18 19:51:44 -0400

    add user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name, :email
+  attributes :id, :username, :first_name, :last_name, :email, :movies, :tech_stories, :most_shareds
 end
