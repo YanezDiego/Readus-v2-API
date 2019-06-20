310171f7882935f6c50064593c3966b4f344432c
Diego Yanez <diegosyanez92@gmail.com>
2019-06-20 15:42:28 -0400

    add correct relationship for user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name, :email, :movies, :tech_stories, :most_shareds
+  attributes :id, :username, :first_name, :last_name, :email, :movies, :techstories, :mostshareds
 end
